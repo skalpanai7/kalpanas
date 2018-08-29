b1ceac02d4d252f4d84d8068fe98040db92bb6ea
skalpanai7 <41998322+skalpanai7@users.noreply.github.com>
2018-08-29 16:25:57 +0530

    Update number.py

--- number.py
+++ number.py
@@ -1,7 +1,7 @@
-number,rep=map(int,raw_input().split())
+number,repeat=map(int,raw_input().split())
 list=[int(x) for x in raw_input().split()]
 c=0
 for i in range(number):
-	if list[i]==rep:
+	if list[i]==repeat:
 		c=c+1
 print c
